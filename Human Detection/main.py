235a014a748a5fae4884a1e267422931f10a0609
nick <nordstromn@wit.edu>
2021-06-10 13:35:37 -0400

    setting up basic structure

---
+++ Human Detection/main.py
@@ -0,0 +1,26 @@
+"""
+@Author
+Nicholas Nordstrom
+
+main.py
+    runs
+
+"""
+import cv2
+import argparse
+
+
+def recieve_frame():
+    pass
+
+
+def process_frame():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
