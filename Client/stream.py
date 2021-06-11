2609d9811861bf9ae8fc69a1e6d69aac2a1aa227
koripain <korileepainchaud@gmail.com>
2021-06-11 21:05:27 +0100

    Changed the Clients streaming file to use the picamera library

--- Client/stream.py
+++ Client/stream.py
@@ -1,21 +1,32 @@
-import cv2
+
 import numpy as np
 import socket
 import sys
 import pickle
 import struct
+from picamera import PiCamera
+from picamera.array import PiRGBArray
+import time
 
-cap=cv2.VideoCapture(0)
+camera = PiCamera()
+camera.resolution = (1280, 720)
+camera.framerate = 10
+raw_capture = PiRGBArray(camera, size=(1280, 720))
+
 clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-clientsocket.connect(('localhost',8089))
+clientsocket.connect(('192.168.0.22',8089))
 
-while True:
-    ret,frame=cap.read()
+time.sleep(0.1)
+
+for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
+    image = frame.array
     # Serialize frame
-    data = pickle.dumps(frame)
+    data = pickle.dumps(image)
 
     # Send message length first
     message_size = struct.pack("L", len(data)) ### CHANGED
 
     # Then data
     clientsocket.sendall(message_size + data)
+
+    raw_capture.truncate(0)
