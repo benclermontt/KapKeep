7bf3ef5766e686b825db3c95fb52fcd5a0c565d4
Ben Clermont <31889040+benclermontt@users.noreply.github.com>
2021-06-14 16:59:19 -0400

    Added gamma normalization via a gamma table and applied it to stream adjusted_image.
    Also added a small rant/plan for upgrading our socket server to not use pickle, allowing us to go over 5fps. However that is easy to do later when we need to.
    
    TODO:
    Finish first step of HOG and normalize the color of the feed

--- Client/stream.py
+++ Client/stream.py
@@ -1,4 +1,27 @@
+"""
+Images are just a bunch of numbers, in a certain order, which says the intensity of each color at each location
+If you use a specific ordering, you can just write down the bytes representing that data and send it as a string of bits, over a socket
+So for example, if I have the array:
+[[ 0, 1 ],
+   [ 1, 0]]
+And I want to represent that in a byte string
+Maybe first I say, ok when I pack this I'll go left to right, then start at the top row and go down
+So, my string representation of that array is:
+"0110"
+If on the other side we knew this was a 2x2 array, and the order it was packed, we can read 4 numbers from the socket which maybe also contains the next frame:
+011011001011
+Then, re-pack it with that order:
+[[ 0, 1 ],
+   [ 1, 0]]
+In your case the big difference is an extra step. You have 3D arrays R, G, B. But if you tell numpy to pack it in 'C' order, it decides all that crap for you
+Then it converts whatever datatype to binary and you have a "byte length" to read on the other side!
+But the argument for a delimiter comes when maybe you lose a number. You choose a delimiter which is long enough and never appears in your message, so maybe "abc" delimits a new frame. Every time you take a character off, you check if the next 3 are "abc":
+Say I was reading a frame with our order from before and I read ahead into the socket and see:
+"011abc1100"
+That first part is obviously a fucked up frame since it's 3 long not four! So we can throw it out and move on with our lives unlike before, where without the delimiter you lose track of what the start of the message even is
+This is much less of a requirement in fixed length messaging but it's still a nice to have. It comes at computational cost
 
+"""
 import numpy as np
 import socket
 import sys
@@ -13,8 +36,8 @@ camera.resolution = (1280, 720)
 camera.framerate = 10
 raw_capture = PiRGBArray(camera, size=(1280, 720))
 
-clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-clientsocket.connect(('192.168.0.22',8089))
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientsocket.connect(('192.168.0.22', 8089))
 
 time.sleep(0.1)
 
@@ -24,7 +47,7 @@ for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port
     data = pickle.dumps(image)
 
     # Send message length first
-    message_size = struct.pack("L", len(data)) ### CHANGED
+    message_size = struct.pack("L", len(data))
 
     # Then data
     clientsocket.sendall(message_size + data)
--- Server/stream.py
+++ Server/stream.py
@@ -1,45 +1,63 @@
 import pickle
 import socket
 import struct
-
+import numpy as np
 import cv2
 
 HOST = ''
 PORT = 8089
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print('Socket created')
 
-s.bind((HOST, PORT))
-print('Socket bind complete')
-s.listen(10)
-print('Socket now listening')
+def adjust_gamma(image, gamma=1.0):
+    inverse_gamma = 1 / gamma
 
-conn, addr = s.accept()
+    gamma_table = np.array([((i / 255.0) ** inverse_gamma) * 255
+                            for i in np.arange(0, 256)]).astype('uint8')
 
-data = b'' ### CHANGED
-payload_size = struct.calcsize("L") ### CHANGED
+    return cv2.LUT(image, gamma_table)
 
-while True:
 
-    # Retrieve message size
-    while len(data) < payload_size:
-        data += conn.recv(4096)
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print('Socket created')
 
-    packed_msg_size = data[:payload_size]
-    data = data[payload_size:]
-    msg_size = struct.unpack("L", packed_msg_size)[0] ### CHANGED
+    s.bind((HOST, PORT))
+    print('Socket bind complete')
+    s.listen(10)
+    print('Socket now listening')
 
-    # Retrieve all data based on message size
-    while len(data) < msg_size:
-        data += conn.recv(4096)
+    conn, addr = s.accept()
 
-    frame_data = data[:msg_size]
-    data = data[msg_size:]
+    data = b''
+    payload_size = struct.calcsize("L")
 
-    # Extract frame
-    frame = pickle.loads(frame_data)
+    while True:
 
-    # Display
-    cv2.imshow('frame', frame)
-    cv2.waitKey(1)
+        # Retrieve message size
+        while len(data) < payload_size:
+            data += conn.recv(4096)
+
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack("L", packed_msg_size)[0]
+
+        # Retrieve all data based on message size
+        while len(data) < msg_size:
+            data += conn.recv(4096)
+
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
+
+        # Extract frame
+        frame = pickle.loads(frame_data)
+
+        adjusted_frame = adjust_gamma(frame, 2)
+
+        # Display
+        cv2.imshow('frame', frame)
+        cv2.imshow('adjusted_frame', adjusted_frame)
+        cv2.waitKey(1)
+
+
+if __name__ == '__main__':
+    main()
